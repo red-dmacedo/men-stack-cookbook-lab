16960d9373aedae50c3480a37f7ad3c85d6269cb
dmacedo <devinm3333@outlook.com>
2025-07-23 12:40:35 -0700

    added recipe schema

---
+++ models/recipe.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const ingredientSchema = mongoose.Schema({
+  name: { type: String, required: true, },
+});
+
+const recipeSchema = mongoose.Schema({
+  name: { type: String, required: true, },
+  instructions: { type: String, required: false, },
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true, },
+  ingredients: { type: [mongoose.Schema.Types.ObjectId], required: false, },
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+module.exports = Recipe;
