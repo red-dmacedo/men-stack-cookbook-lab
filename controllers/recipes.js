c9c737137c719e476e3080eef02672ae262fd75c
dmacedo <devinm3333@outlook.com>
2025-07-23 13:05:32 -0700

    created navbar

---
+++ controllers/recipes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+const Recipe = require('../models/recipe.js');
+
+// routes
+
+
+module.exports = router;
